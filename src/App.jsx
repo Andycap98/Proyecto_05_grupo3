0d31b425d4e3c4e33481c69352d75ef08ebfed5f
PabloApasa <143457610+PabloApasa@users.noreply.github.com>
2025-10-22 21:21:50 -0300

    Correciones del Formluario

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
-import EstudiantesForm from './assets/components/Formulario/EstudiantesForm'
-import AtraparEstrellas from './assets/components/AtraparEstrellas/AtraparEstrellas'
-
+import FormJuego from './assets/components/Formulario/FormJuego';
 
 function App() {
   const [count, setCount] = useState(0)
   const [estudiantes, setEstudiantes] = useState([]);
 
   const agregarEstudiante = (estudiante) => {
-  setEstudiantes([...estudiantes, estudiante])
+    setEstudiantes([...estudiantes, estudiante])
   };
 
   return (
     <Container>
-      <h1>Formulario de Estudiantes</h1>
-      <EstudiantesForm/>
-      <AtraparEstrellas/>
+
+      <FormJuego />
+
+      <h1> Lista de juegos </h1>
+
     </Container>
   )
 }
